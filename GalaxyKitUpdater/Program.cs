358c668c2119093fb3cfd57560b546fe946526c5
Neoxy <1007050740@qq.com>
2022-08-13 21:17:32 +0800

    添加项目文件。

---
+++ GalaxyKitUpdater/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace GalaxyKitUpdater
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String url = "https://api.github.com/repos/allstars-org/galaxykit/releases/latest";
+            Console.WriteLine("GalaxyKitUpdater 1.0.0 Made by Neoxy");
+            Console.WriteLine("Loading files...");
+            try
+            {
+                WebClient wc = new WebClient();
+                String raw = wc.DownloadString("");
+
+            }catch(Exception error)
+            {
+                Console.WriteLine("Something went wrong.");
+                Console.WriteLine(error.Message.ToString());
+            }
+        }
+    }
+}
